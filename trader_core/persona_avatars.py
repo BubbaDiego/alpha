a39f6a55362a3f2c5d65426561a01b0b7031985c
bubba <bubba.diego@gmail.com>
2025-06-03 16:56:23 -0700

    trader stuff

---
+++ trader_core/persona_avatars.py
@@ -0,0 +1,10 @@
+# Avatar registry: emoji and image avatars for persona selection
+
+AVATARS = {
+    "fox": "🦊",
+    "rocket": "🚀",
+    "panther": "🐆",
+    "r2vault": "/static/images/r2vault.jpg",
+    "landovault": "/static/images/landovault.jpg",
+    "vadervault": "/static/images/vadervault.jpg",
+}
