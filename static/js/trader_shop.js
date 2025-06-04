7354d1a7c5a2359eaea6268f094d6e5518c3343d
BubbaDiego <106400961+BubbaDiego@users.noreply.github.com>
2025-06-03 20:25:26 -0700

    Fix avatar HTML rendering

--- static/js/trader_shop.js
+++ static/js/trader_shop.js
@@ -54,9 +54,9 @@ function loadAvatars() {
   select.addEventListener("change", () => {
     const val = select.value;
     if (val.startsWith("/static/")) {
-      preview.innerHTML = `<img src="\${val}" style="height: 60px; border-radius: 50%;">`;
+      preview.innerHTML = `<img src="${val}" style="height: 60px; border-radius: 50%;">`;
     } else {
-      preview.innerHTML = `<span style="font-size: 1.5rem;">\${val}</span>`;
+      preview.innerHTML = `<span style="font-size: 1.5rem;">${val}</span>`;
     }
   });
 }
@@ -103,9 +103,9 @@ function loadTraders() {
 
         let avatarHTML = "";
         if (trader.avatar?.startsWith("/static/")) {
-          avatarHTML = `<img src="\${trader.avatar}">`;
+          avatarHTML = `<img src="${trader.avatar}">`;
         } else if (trader.avatar) {
-          avatarHTML = `<div style="font-size: 1.5rem;">\${trader.avatar}</div>`;
+          avatarHTML = `<div style="font-size: 1.5rem;">${trader.avatar}</div>`;
         }
 
         card.innerHTML = `
